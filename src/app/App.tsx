8fc4dd63d079b95016b51328363fb40ed3b5714f
Andrew <ak36794ca@gmail.com>
2026-02-21 06:23:13 +0200

    implement HashRoter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,16 @@ import { fontSizeService, themeService } from '@/shared/lib';
 import { themeSettingsService } from '@/features/ThemeSettings';
 import '@/app/styles/index.css';
 import '@/app/styles/variables.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 export const App = () => {
     fontSizeService.init();
     themeService.init();
     themeSettingsService.init();
     return (
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter>
             <AppRouter />
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
